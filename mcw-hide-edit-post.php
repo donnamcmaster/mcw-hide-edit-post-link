d887c0c9b93d473bb665a076d775d6293cbc43bf
Donna McMaster <donna@donnamcmaster.com>
2018-08-15 14:56:39 -0700

    first commit

---
+++ mcw-hide-edit-post.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: McWebby Hide Edit Post Links
+Plugin URI: http://www.donnamcmaster.com/
+Description: Temporarily hides the edit links so that you can view the page without them. 
+Version: 00.01.00
+Author: Donna McMaster
+Author URI: http://www.donnamcmaster.com/
+License: GNU GPU v3
+*/
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ *	Stylesheets and scripts
+ */
+add_action( 'wp_enqueue_scripts', function () {
+?>
+<style type="text/css">
+	a.post-edit-link {
+		display: none !important;
+	}
+</style>
+<?php
+}, 99 );
